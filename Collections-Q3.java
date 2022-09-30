678707d48730981d6057176c69f28da8cbe41ea4
YogeshTTN <114072142+YogeshTTN@users.noreply.github.com>
2022-09-30 10:19:07 +0530

    Update Collections-Q3.java

--- Collections-Q3.java
+++ Collections-Q3.java
@@ -1,51 +1,76 @@
+import java.util.Stack;
 import java.util.*;
-class sortBysalarycomparator implements Comparator<Employeee>{
-    public int compare(Employeee e1, Employeee e2){
-        if(e1.salary<e2.salary)
+
+public class question3 {
+    static Stack<Integer> st = new Stack<Integer>();
+    static Integer minEle = Integer.MAX_VALUE;
+
+    public static void pushEle(int num) {
+        if (st.empty()) {
+            minEle = num;
+            st.push(num);
+            return;
+        }
+        if (num < minEle) {
+            st.push(2 * num - minEle);
+            minEle = num;
+        } else
+            st.push(num);
+
+    }
+
+    public static void popEle() {
+        if (st.empty()) {
+            System.out.println("Can't pop the element");
+            return;
+        }
+        int top = st.pop();
+        if (top < minEle) {
+            minEle = 2 * minEle - top;
+        }
+    }
+
+    public static int getMin() {
+        if (st.empty())
             return -1;
-        else if(e1.salary>e2.salary)
-            return 1;
-        return 0;
-    }
-}
-class Employeee implements Comparable<Employeee>{
-    String first_name,last_name;
-    int age,salary;
-    public Employeee(String f,String l,int a,int s){
-        this.first_name = f;
-        this.last_name = l;
-        this.age = a;
-        this.salary = s;
+        return minEle;
     }
 
-    @Override
-    public int compareTo(Employeee e) {
-        if(this.first_name.compareTo(e.first_name)!=0){
-            return this.first_name.compareTo(e.first_name);
-        }
-        else
-            return this.last_name.compareTo(e.last_name);
-    }
-}
-public class question2 {
-    public static void main(String[] args){
-        ArrayList<Employeee> al = new ArrayList<Employeee>();
-        Employeee e;
-        al.add(new Employeee("Yogesh","1",21,10000));
-        al.add(new Employeee("Yogesh","2",22,20000));
-        al.add(new Employeee("Yogesh","3",23,30000));
-        al.add(new Employeee("Yogesh","4",24,40000));
-        System.out.println("Sorting by name:-");
-        Collections.sort(al);
-        for(Employeee e1 : al){
-            System.out.println(e1.first_name+" "+e1.last_name+" "+e1.age+" "+e1.salary);
-        }System.out.println();
-        System.out.println("Sorting by salary:-");
-        Collections.sort(al,new sortBysalarycomparator());
-        for(Employeee e1 : al){
-            System.out.println(e1.first_name+" "+e1.last_name+" "+e1.age+" "+e1.salary);
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        while (true) {
+            
+            System.out.println("1 : Push Element");
+            System.out.println("2 : Pop Element");
+            System.out.println("3 : Get Min Element");
+            System.out.println("4 : Exit");
+            
+            System.out.print("Enter any above choices :  ");
+            int n = sc.nextInt();
+
+            switch (n) {
+                case 1:
+                    System.out.println("Enter the number : ");
+                    int num = sc.nextInt();
+                    System.out.println();
+                    pushEle(num);
+                    break;
+                case 2:
+                    popEle();
+                    break;
+                case 3:
+                    System.out.println("Minimum number is : " + getMin() + "\n");
+                    
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("invalid Choice");
+                    break;
+            }
+        }
 
     }
 
